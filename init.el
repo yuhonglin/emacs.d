62b36c64328aad3af139f0c298fac237874fff64
Honglin Yu <honglin.yu@anu.edu.au>
2016-09-16 11:20:50 +1000

    load ess-jags-mode

--- init.el
+++ init.el
@@ -83,6 +83,7 @@
 (use-package ess
   :config
   (setq inferior-R-args "--no-restore-history --no-save")
+  (require 'ess-jags-d)
   :ensure t)
 
 (use-package elpy
@@ -113,3 +114,17 @@
   :config
   (load-theme 'moe-dark t)
   :ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("dbb643699e18b5691a8baff34c29d709a3ff9787f09cdae58d3c1bc085b63c25" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
