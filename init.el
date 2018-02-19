6b12e84f190055c2aa5470646725afeaa8493db5
Honglin Yu <yuhonglin1986@gmail.com>
2018-02-19 11:26:15 +1100

    Update init.el

--- init.el
+++ init.el
@@ -138,17 +138,3 @@
 (matlab-mode-common-setup)
 (setq matlab-server-executable "/home/lin/local/MatlabR2015a/bin/matlab")
 (put 'upcase-region 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (company-jdee ecb jdee company-emacs-eclim eclim emacs-eclim org-bullets request stan-snippets stan-mode clang-format web-mode elpy selected ess irony-eldoc flycheck-irony company-irony irony helm yasnippet magit auctex company flycheck cmake-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
